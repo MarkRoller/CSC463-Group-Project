c76bc138d6f91e0c5f1129fdbc67ec44d720968c
MarkRoller <eng.mroller@gmail.com>
2015-12-08 11:18:35 -0800

    Final version of network generation. Approach to problem has changed since last commit

--- NetworkGeneration.py
+++ NetworkGeneration.py
@@ -1,9 +1,9 @@
 # # #
 # Takes input to create a number of networks that we can later analyze
 #
-# Usage: python NetworkGeneration.py (Max Nodes) (Max Connections Per Node) (Max Distance Between Connected Nodes) (Number Of Networks To Create) (Name Of Output File)
+# Usage: python NetworkGeneration.py (Max Nodes) (Max Distance Between Nodes) (Number Of Networks To Create) (Name Of Output File)
 #
-# @version 1 - Modified November 16 2015 12:30 PM
+# @version 1 - Modified December 8 2015 11:00 AM
 # @python-version 2.7
 # #
 
@@ -11,61 +11,51 @@ import sys
 from random import randint
 
 def processor(inputs):
+	minNodes = 3
+	minDist = 1
+	minChance = 25
+	maxChance = 75
+
 	try:
 		maxNodes = int(inputs[0])
-		maxConn = int(inputs[1])
-		maxDist = int(inputs[2])
-		numNetworks = int(inputs[3])
-		outputName = inputs[4]
+		maxDist = int(inputs[1])
+		numNetworks = int(inputs[2])
+		outputName = inputs[3]
 	except:
 		print("Missing Input.")
 		sys.exit(1)
 
 	with open("./" + outputName, "w") as outputFile:
 		while numNetworks > 0:
+			conChance = randint(minChance,maxChance)
+			numNodes = randint(minNodes,maxNodes)
+			outMatrix = [[0 for x in range(numNodes)] for x in range(numNodes)]
+
+			numConnections = 0
+			for row in range(0,numNodes):
+				for col in range(row,numNodes):
+					if row == col:
+						continue
+
+					outMatrix[row][col] = randint(minDist,maxDist)
+
+					if randint(0,100) < conChance:
+						outMatrix[col][row] = 1
+						numConnections = numConnections+1
+
+			if numConnections == 0:
+				col = randint(0,numNodes-2)
+				row = randint(col+1,numNodes-1)
+				outMatrix[row][col] = 1
 			
-			outNodes = randint(1,maxNodes)
-			outMatrix = [[-1 for x in range(outNodes)] for x in range(outNodes)]
-			numNodes = outNodes
+			for row in range(0,numNodes):
+				for col in range(0,numNodes):
+					outputFile.write(str(outMatrix[row][col]))
 
-			while outNodes > 0:
-				outConnections = randint(1,maxConn)
-				rowCounter = 0
-
-				while outConnections > 0:
-					connection = randint(1,numNodes)
-
-					if outMatrix[outNodes-1][connection-1] == -1:
-						if outNodes == connection:
-							outMatrix[outNodes-1][connection-1] = 0
-							outMatrix[connection-1][outNodes-1] = 0
-						else:
-							outTemp = randint(1,maxDist)
-							outMatrix[outNodes-1][connection-1] = outTemp
-							outMatrix[connection-1][outNodes-1] = outTemp
-
-						outConnections = outConnections-1
-					else:
-						rowCounter = rowCounter+1
-
-					if rowCounter == numNodes*2:
-						break
-				
-				outNodes = outNodes-1
-			
-			tempNodes = numNodes
-			while tempNodes > 0:
-				temp2Nodes = numNodes
-				while temp2Nodes > 0:
-					outputFile.write(str(outMatrix[tempNodes-1][temp2Nodes-1]))
-					temp2Nodes = temp2Nodes-1
-
-					if temp2Nodes == 0:
+					if col == numNodes-1:
 						outputFile.write("\n")
 					else:
 						outputFile.write(",")
-
-				tempNodes = tempNodes-1
 
 			outputFile.write("<end>\n")
 			numNetworks = numNetworks-1
