fef25d5717d52f0546fb08a6f68d428c54cddbc8
Christina Gee <christinagee25@gmail.com>
2015-12-08 16:15:56 -0800

    PowerControl method but left in my super fun print statements

--- NetworkAnalyzer.py
+++ NetworkAnalyzer.py
@@ -10,10 +10,71 @@
 import sys
 
 def PowerControl(network,numNodes):
-	for col in range(0,numNodes-2):
-		for row in range(1,numNodes-1):
+
+	for line in network:
+		print line
+
+	# dictionary of nodes and their transmitting power 
+	nodes = {}
+	for row in range(0,numNodes):
+		nodes[row] = 0
+
+	for row in range(1,numNodes):
+		for col in range(0,row):
+			print network[row][col]
 			if network[row][col] == 1:
-				dummyVariable = 0
+				if network[col][row] > nodes[row]:
+					nodes[row] = network[col][row]
+
+			if network[row][col] == -1:
+				if network[col][row] > nodes[col]:
+					nodes[col] = network[col][row]
+				
+	print nodes
+
+
+	# for each node entry in the dictionary, check to see if it is transmitting 
+	for entry in nodes:
+		receiving = []
+		# if a node entry has a value of 0, then the node is not transmitting
+		if nodes[entry] == 0:
+			continue
+		else:
+			# the node is transmitting and here is where we have to do the comparisons	
+			for row in range(1,numNodes):	
+				if network[row][entry] == -1:
+					receiving.append(row)
+			for col in range(0,entry):
+				if network[entry][col] == 1:
+					receiving.append(col)
+
+
+		print "receiving"
+		print receiving				
+		
+		for y in receiving:
+			nodes_reached = []
+			for x in nodes:
+				if nodes[x] == 0:
+					continue
+				print "network entry x"
+				print network[x][y]
+				print "node entry"
+				print nodes[x]
+				if network[x][y] <= nodes[x]:
+				# the node is within reach	
+					if x == y:
+						continue
+					nodes_reached.append(x)
+		print "nodes reached just to see"
+		print nodes_reached
+
+		#print receiving
+
+
+
+
+	return "end of function"	
 
 				# TODO:
 				# Add to dict
@@ -52,7 +113,7 @@ def Processor(inputs):
 
 				if line.split("\n")[0] == "<end>":
 					getNodes = 0
-					powerResult = PowerControl(inMatrix,numNodes)
+					print PowerControl(inMatrix,numNodes)
 					regularResult = RegularControl(inMatrix,numNodes)
 
 					# TODO: ADD OUTPUT TO FILE
@@ -63,6 +124,7 @@ def Processor(inputs):
 					inMatrix[rowPosition][colPosition] = int(value)
 					colPosition = colPosition+1
 				rowPosition = rowPosition+1
+
 
 if __name__ == '__main__':
     Processor(sys.argv[1:])
